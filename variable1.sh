18691b92fcc84c2733fa5c612eac3974927948f7
Godwin <gijewemen@gmail.com>
2022-10-23 13:15:57 -0400

    ifstatements

---
+++ variable1.sh
@@ -0,0 +1,15 @@
+    #Author : Bobby
+    #Date : Oct 2022
+
+## ---------- variables allow to contain values that we want to use several times in our scripts -----------------
+
+#In this script, we can create 4 variables ...
+
+FIRST_NAME=Bobby
+LAST_NAME=Dave
+COLOR=Black
+AGE=19
+
+echo "$FIRST_NAME is a very hard working boy"
+echo "His last name is $LAST_NAME and his favorite color is $COLOR"
+echo "$FIRST_NAME is $AGE years old"
